22de4a759b99a0a649e3d3fe4c66ea72830be4e3
myyrakle <sssang97@naver.com>
2024-10-13 01:21:37 +0900

    [#1] 코멘트 정리

--- src/main.rs
+++ src/main.rs
@@ -115,24 +115,20 @@ async fn handle_proxy_request(
 async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
     let addr = SocketAddr::from(([0, 0, 0, 0], 3000));
 
-    // We create a TcpListener and bind it to the address we want to listen on
+    // create TCP listener bound to the address
     let listener = TcpListener::bind(addr).await?;
 
     println!("Listening on http://{}", addr);
 
-    // We start a loop to continuously accept incoming connections
+    // accept incoming connections
     loop {
         let (stream, _) = listener.accept().await?;
 
-        // Use an adapter to access something implementing `tokio::io` traits as if they implement
-        // `hyper::rt` IO traits.
         let io = TokioIo::new(stream);
 
         // Spawn a tokio task to serve multiple connections concurrently
         tokio::task::spawn(async move {
-            // Finally, we bind the incoming connection to our `hello` service
             if let Err(err) = http1::Builder::new()
-                // `service_fn` converts our function in a `Service`
                 .serve_connection(io, service_fn(handle_proxy_request))
                 .await
             {
