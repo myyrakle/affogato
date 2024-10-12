45b4e93a5745f25ce1c463889b6104498b78e003
myyrakle <sssang97@naver.com>
2024-10-13 00:20:01 +0900

    Proxy-Host 획득 구현

--- src/main.rs
+++ src/main.rs
@@ -5,19 +5,69 @@ use http_body_util::Full;
 use hyper::body::Bytes;
 use hyper::server::conn::http1;
 use hyper::service::service_fn;
-use hyper::{Request, Response};
+use hyper::{Request, Response, Uri};
 use hyper_util::rt::TokioIo;
 use tokio::net::TcpListener;
 
-async fn hello(_: Request<hyper::body::Incoming>) -> Result<Response<Full<Bytes>>, Infallible> {
+async fn hello(
+    mut request: Request<hyper::body::Incoming>,
+) -> Result<Response<Full<Bytes>>, Infallible> {
+    let headers = request.headers_mut();
+
+    let Some(proxy_target) = headers.remove("Proxy-Host") else {
+        return Ok(Response::builder()
+            .status(400)
+            .body(Full::new(Bytes::from("Proxy-Host header is missing")))
+            .unwrap());
+    };
+
+    let Ok(proxy_target) = proxy_target.to_str() else {
+        return Ok(Response::builder()
+            .status(400)
+            .body(Full::new(Bytes::from(
+                "Proxy-Host header is not a valid string",
+            )))
+            .unwrap());
+    };
+
+    let Ok(mut proxy_target_uri) = proxy_target.parse::<Uri>() else {
+        return Ok(Response::builder()
+            .status(400)
+            .body(Full::new(Bytes::from(
+                "Proxy-Host header is not a valid URL",
+            )))
+            .unwrap());
+    };
+
+    let uri = request.uri();
+    let path = uri.path();
+    let raw_query = uri.query();
+
+    let mut request_uri =
+        String::with_capacity(proxy_target.len() + path.len() + raw_query.unwrap_or("").len());
+
+    request_uri.push_str(proxy_target);
+    request_uri.push_str(path);
+
+    if let Some(raw_query) = raw_query {
+        request_uri.push('?');
+        request_uri.push_str(raw_query);
+    }
+
+    let mut proxy_request = Request::builder()
+        .method(request.method())
+        .uri(request_uri)
+        .body(request.body())
+        .unwrap();
+
     Ok(Response::new(Full::new(Bytes::from("Hello, World!"))))
 }
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
-    let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
+    let addr = SocketAddr::from(([0, 0, 0, 0], 3000));
 
-    // We create a TcpListener and bind it to 127.0.0.1:3000
+    // We create a TcpListener and bind it to the address we want to listen on
     let listener = TcpListener::bind(addr).await?;
 
     // We start a loop to continuously accept incoming connections
