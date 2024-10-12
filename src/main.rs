3ea749e41c4cab72f9e562f0b86c34e73d6a0c1a
myyrakle <sssang97@naver.com>
2024-10-13 00:57:47 +0900

    proxy 로직 구현

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,11 @@
 use std::convert::Infallible;
 use std::net::SocketAddr;
 
-use http_body_util::Full;
+use http_body_util::{BodyExt, Full};
 use hyper::body::Bytes;
 use hyper::server::conn::http1;
 use hyper::service::service_fn;
-use hyper::{Request, Response, Uri};
+use hyper::{HeaderMap, Request, Response};
 use hyper_util::rt::TokioIo;
 use tokio::net::TcpListener;
 
@@ -14,6 +14,7 @@ const PROXY_HOST_HEADER: &str = "Proxy-Host";
 async fn hello(
     mut request: Request<hyper::body::Incoming>,
 ) -> Result<Response<Full<Bytes>>, Infallible> {
+    // 1. get 'Proxy-Host' header from request
     let headers = request.headers_mut();
 
     let Some(proxy_target) = headers.remove(PROXY_HOST_HEADER) else {
@@ -32,37 +33,83 @@ async fn hello(
             .unwrap());
     };
 
-    let Ok(mut proxy_target_uri) = proxy_target.parse::<Uri>() else {
+    // 2. prepare request
+
+    // 2.1. get request method
+    let method = request.method().to_owned();
+
+    // 2.2. get request headers
+    let mut request_headers = HeaderMap::new();
+    std::mem::swap(&mut request_headers, request.headers_mut());
+
+    // 2.3 generate request URI for proxy
+    let request_uri = {
+        let uri = request.uri();
+        let path = uri.path();
+        let raw_query = uri.query();
+        let mut request_uri =
+            String::with_capacity(proxy_target.len() + path.len() + raw_query.unwrap_or("").len());
+
+        request_uri.push_str(proxy_target);
+        request_uri.push_str(path);
+
+        if let Some(raw_query) = raw_query {
+            request_uri.push('?');
+            request_uri.push_str(raw_query);
+        }
+
+        request_uri
+    };
+
+    // 2.4. get request body
+    let Ok(request_body) = request.into_body().collect().await.map(|body| {
+        let bytes = body.to_bytes().to_vec();
+        unsafe { String::from_utf8_unchecked(bytes) }
+    }) else {
         return Ok(Response::builder()
             .status(400)
-            .body(Full::new(Bytes::from(
-                "Proxy-Host header is not a valid URL",
-            )))
+            .body(Full::new(Bytes::from("Failed to read request body")))
             .unwrap());
     };
 
-    let uri = request.uri();
-    let path = uri.path();
-    let raw_query = uri.query();
+    // 3. send request to proxy
+    let Ok(client) = reqwest::ClientBuilder::new().build() else {
+        return Ok(Response::builder()
+            .status(400)
+            .body(Full::new(Bytes::from("Failed to create a reqwest client")))
+            .unwrap());
+    };
 
-    let mut request_uri =
-        String::with_capacity(proxy_target.len() + path.len() + raw_query.unwrap_or("").len());
+    let proxy_request = client
+        .request(method, request_uri)
+        .body(request_body)
+        .headers(request_headers);
 
-    request_uri.push_str(proxy_target);
-    request_uri.push_str(path);
+    let proxy_result = proxy_request.send().await;
 
-    if let Some(raw_query) = raw_query {
-        request_uri.push('?');
-        request_uri.push_str(raw_query);
+    // 4. return response from proxy to client
+    match proxy_result {
+        Ok(response) => {
+            let mut response_builder = Response::builder().status(response.status());
+
+            let headers = response_builder.headers_mut().unwrap();
+
+            for (key, value) in response.headers() {
+                headers.insert(key, value.clone());
+            }
+
+            let body = response.bytes().await.unwrap();
+
+            Ok(response_builder.body(Full::new(body)).unwrap())
+        }
+        Err(err) => Ok(Response::builder()
+            .status(500)
+            .body(Full::new(Bytes::from(format!(
+                "Failed to send request: {}",
+                err
+            ))))
+            .unwrap()),
     }
-
-    let mut proxy_request = Request::builder()
-        .method(request.method())
-        .uri(request_uri)
-        .body(request.body())
-        .unwrap();
-
-    Ok(Response::new(Full::new(Bytes::from("Hello, World!"))))
 }
 
 #[tokio::main]
